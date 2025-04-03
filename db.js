0d9cc70d83ba0a2768ad8d92f57b989e72b851f8
granitespinoza <112722319+granitespinoza@users.noreply.github.com>
2025-04-03 12:30:00 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,3 +1,6 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/database.db');  // Aquí debes crear tu base de datos
+
 const initDB = () => {
   db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
@@ -24,3 +27,5 @@ const initDB = () => {
     });
   });
 };
+
+module.exports = { db, initDB };  // Asegúrate de exportar tanto db como initDB
